1e3138632e3886251c6297cd6b33600532bff5da
Mattias <stahr@kth.se>
2018-02-21 11:20:26 +0100

    cd

--- mipslabwork.c
+++ mipslabwork.c
@@ -1,5 +1,3 @@
-// Session ID: b3a28c59-4e43-43fa-8009-6b914f08bf8b
-
 /* mipslabwork.c
 
    This file written 2015 by F Lundevall
@@ -15,15 +13,6 @@
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
-#if TMR2PERIOD > 0xffff
-#error "Timer period is too big. No more than 16 bits can be used for the timer"
-#endif
-#define   TMR2PERIOD ((80000000 / 256) / 10)  // = 31250 < 65 536 (=2^16)
-
-int TimeOutCount = 0;
-int globalTimeOutCount = 0;
-
-int prime = 1234567;
 
 int mytime = 0x5957;
 
@@ -38,20 +27,9 @@ volatile int* initPORTE = (volatile int*) 0xbf886110;
 
 
 /* Interrupt Service Routine */
-void user_isr( void ) {
-
-TimeOutCount++;
-
-if (TimeOutCount == 10){
- time2string( textstring, mytime );
- display_string( 3, textstring );
- display_update();
- tick( &mytime );
- TimeOutCount = 0;      //nollställ variabel
-
-}
-  IFSCLR(0) = 0x100;    // Nollställ interuptflaggan!
-
+void user_isr( void )
+{
+  return;
 }
 
 /* Lab-specific initialization goes here */
@@ -74,44 +52,7 @@ volatile int* initTRISE = (volatile int*) 0xbf886100;
 
 TRISD = 0xfe0;
 
-  // Initiera timer 2 för timeouts 100ms Är en typ B timer
 
-  T2CON = 0x70;          // 0111 0000 Vi sätter bit 6:4 till 111 prescale 1:256
-                        // set prescaler at 1:1, internal clock source
-  TMR2 = 0x0;           // Clear timer register
-  PR2 = TMR2PERIOD;         // Load period register
-
-  T2CONSET = 0x8000;         // Start timer
-
-
-
-    // Slår på interupts för timer 2.
-    // LÄS PÅ OM IPC OCH IEC!!!!!!!
-    // Använder pekare.
-
-    int *IEC = 0xbf881060;
-    int *IPC2 = 0xbf8810b0;
-
-
-
-    *IEC = 0x100;     //Vi sätter bit #9 (index 8) se bild sid 90.
-    // Interupt Priority Control
-    // http://ww1.microchip.com/downloads/en/DeviceDoc/61143H.pdf Sid 90.
-    // Bitar 4:2 sätter priotitet 0-7 där 0-3 är Subpriority.
-
-
-    *IPC2 = *IPC2 | 0x10;   //Vi sätter prioriteret 4 genom att endast bit #5 (index 4)
-
-/*
-    IPC(2) = IPC(2) | 0x10;
-    // set bit no 8 to enable interupt
-    IEC(0) = 0x100;
-    // calling interupt from labwork.S
-*/
-    enable_interrupt();
-
-
-    // IEC0SET = 0x00000100;
 
 
 
@@ -119,8 +60,39 @@ TRISD = 0xfe0;
 }
 
 /* This function is called repetitively from the main program */
-void labwork( void ) {
-  prime = nextprime( prime );
-  display_string( 0, itoaconv( prime ) );
+void labwork( void )
+{
+
+  delay( 1000 );
+
+  time2string( textstring, mytime );  // mytime är hex t ex 0x5957
+    // nollställer rätt bitar ex 8:5 i mytime. Vi lägger på värdet från getsw i dem bitarna.
+
+    if ( getbtns() == 1){
+      mytime = ((mytime & (~0xf0)) |  (getsw() << 4 ));
+      time2string( textstring, mytime );
+    }
+
+    if ( getbtns() == 2 ){
+      mytime = ((mytime & (~0xf00)) |  (getsw() << 8));
+      time2string( textstring, mytime );
+    }
+
+    if ( getbtns() == 4 ){
+      mytime = ((mytime & (~0xf000)) |  (getsw() << 12));
+      time2string( textstring, mytime);
+    }
+
+
+
+  display_string( 3, textstring );
   display_update();
+  tick( &mytime );
+
+  // +1 varje gång labwork anropas.
+  *initPORTE += 1;
+  display_image(96, icon);
+
+
+
 }
